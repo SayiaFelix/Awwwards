33d3249a1dbc6cca6e605ef113ada210d48086d7
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-10 14:16:50 +0300

    testing my profile model

--- awwards/tests.py
+++ awwards/tests.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 from .models import *
 
 
-class ProfileTest(TestCase):
+class ProfileTestClass(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(id = 2, username='cherry')
-        self.profile = Profile.objects.create(user = self.user,bio = 'blow', phone_number= 2356789)
+        self.user = User.objects.create(id = 1, username='cherry')
+        self.profile = Profile.objects.create(user = self.user,bio = 'love her',phone_number= 2356789)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
@@ -14,9 +14,14 @@ class ProfileTest(TestCase):
     def test_save_profile(self):
         self.assertTrue(isinstance(self.profile,Profile))
 
-    def test_search_profile(self):
+    def test_get_profile(self):
         self.profile.save()
         profile = Profile.get_profile()
+        self.assertTrue(len(profile) > 0)
+
+    def test_search_profile(self):
+        self.profile.save()
+        profile = Profile.search_profile('cherry')
         self.assertTrue(len(profile) > 0)
 
 
