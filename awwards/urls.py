d42d0023b194948ea70658b00034cfec3c69c8b7
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-11 11:54:20 +0300

    upload functionality

--- awwards/urls.py
+++ awwards/urls.py
@@ -7,8 +7,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns=[
     url('^$',views.homepage, name='homepage'),
     url('profile/(\d+)', views.user_profile, name='profile'),
-    url('^new/profile$', views.add_user_profile, name='add_profile'),
-    url('^search/', views.search, name='search'),
+    url('new/profile$', views.add_user_profile, name='add_profile'),
+    url('search/', views.search, name='search'),
+    url('upload/', views.update_project, name='upload_project'),
+    url('review/(?P<pk>\d+)',views.add_review,name='review'),
+    url('all/(?P<pk>\d+)', views.all_projects, name='all'),
    
 ]
 
--- awwards/views.py
+++ awwards/views.py
@@ -74,3 +74,51 @@ def search(request):
     else:
         message = "You haven't searched for any project"
         return render(request,'awwards/search.html',{"message":message})
+
+def update_project(request):
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.posted_by = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('homepage')
+            else:
+                form = UploadForm()
+            return render(request,'awwards/upload_project.html',{"user":current_user,"form":form})
+
+def add_review(request,pk):
+    project = get_object_or_404(Projects, pk=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            review = form.save(commit=False)
+            review.project = project
+            review.juror = current_user
+            review.design = design
+            review.usability = usability
+            review.content = content
+            review.save()
+            return redirect('home')
+    else:
+        form = ReviewForm()
+        return render(request,'awwards/review.html',{"user":current_user,"form":form})
+
+
+def all_projects(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    projects = Projects.objects.all().filter(posted_by_id=pk)
+    content = {
+        "profile": profile,
+        'projects': projects,
+    }
+    return render(request, 'profile.html', content)
