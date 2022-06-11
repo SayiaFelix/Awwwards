01e7a437e108033e76deb54d9d674e89f44e62a7
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-11 10:25:32 +0300

    adding user profile

--- awwards/views.py
+++ awwards/views.py
@@ -47,3 +47,17 @@ def user_profile(request,profile_id):
     projects = Projects.objects.filter(profile_id=profile).all()
 
     return render(request,"profile/profile.html",{"profile":profile,"projects":projects})
+
+def add_user_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('homepage')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'profile/new_user_profile.html', {"form": form})
