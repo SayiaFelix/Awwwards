ef54f61c21141daab51d8044818c938a0db37701
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-15 12:43:09 +0300

    Adding API Endpoints

--- awwards/views.py
+++ awwards/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
-from .tokens import account_activation_token
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.core.mail import EmailMessage
