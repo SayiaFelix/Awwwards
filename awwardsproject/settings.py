8820798ea3ef74592fd084b561d72ba637806f0b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-17 12:31:39 +0300

    Adding API Endpoints

--- awwardsproject/settings.py
+++ awwardsproject/settings.py
@@ -119,8 +119,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sayia@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmyzue'
+EMAIL_HOST_USER = 'tjyu@gmail.com'
+EMAIL_HOST_PASSWORD = 'te'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
