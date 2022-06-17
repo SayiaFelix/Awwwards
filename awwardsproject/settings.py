122eaef45b2c0b8259a1c25438821d95c9383600
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-17 12:24:26 +0300

    Adding API Endpoints

--- awwardsproject/settings.py
+++ awwardsproject/settings.py
@@ -120,7 +120,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sayia@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmyierwpdfxqhzue'
+EMAIL_HOST_PASSWORD = 'tmyzue'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
